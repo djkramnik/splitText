ce27f43b1d0c9330ee4654437280e49fdced660e
Hans De Smet <hello@hansdesmet.be>
2020-04-23 10:57:16 +0200

    Refactor: Import splitText from own repo to see latest changes for demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,7 @@
-import splitText from 'split-text';
+import splitText from '../../lib/index';
 
-splitText(document.querySelector('#quote'))
-  .then(revert => {
-    const btn = document.querySelector('button');
-    btn.removeAttribute('disabled');
-    btn.addEventListener('click', revert);
-  });
+splitText(document.querySelector('#quote')).then(revert => {
+  const btn = document.querySelector('button');
+  btn.removeAttribute('disabled');
+  btn.addEventListener('click', revert);
+});
